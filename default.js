d7aaa4f3a4d006469671435224232f9d7aa11ca0
pliddle <9013070+pliddle@users.noreply.github.com>
2025-11-22 14:46:46 +0000

    Create default.js

---
+++ default.js
@@ -0,0 +1,158 @@
+window.HGIOS_SAVED_DATA = {
+  "selections": {
+    "2.3.1.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.1.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "2.3.1.3": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.2.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.2.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.2.3": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.2.4": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.3.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.3.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.3.3.3": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.2.1.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.2.1.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.2.2.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "2.2.2.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "2.2.3.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "2.2.3.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "1.3.1.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "1.3.1.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "1.3.1.3": {
+      "level": 6,
+      "status": "secure"
+    },
+    "1.3.2.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "1.3.2.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "1.3.2.3": {
+      "level": 5,
+      "status": "secure"
+    },
+    "1.3.3.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "1.3.3.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "1.3.3.3": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.1.1.1": {
+      "level": 5,
+      "status": "secure"
+    },
+    "3.1.1.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.1.1.3": {
+      "level": 5,
+      "status": "secure"
+    },
+    "3.1.2.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.1.2.2": {
+      "level": 5,
+      "status": "secure"
+    },
+    "3.1.2.3": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.2.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.2.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.3.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.3.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.4.1": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.4.2": {
+      "level": 6,
+      "status": "secure"
+    },
+    "3.2.4.3": {
+      "level": 6,
+      "status": "secure"
+    }
+  },
+  "comments": {},
+  "timestamp": "2025-11-22T14:45:45.120Z"
+};
